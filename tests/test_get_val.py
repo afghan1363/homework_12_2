a5f2452efa85122047ad4bc4694aeb0befe68afe
Maxim Abavi <afghan1363@ya.kz>
2023-07-29 14:23:31 +0500

    rename test_

---
+++ tests/test_get_val.py
@@ -0,0 +1,10 @@
+from utils.dict import get_val
+
+dict_for_test = {"lang": "Python", "version": "3.11"}
+
+
+def test_get_val():
+    assert get_val(dict_for_test, "lang") == "Python"
+    assert get_val(dict_for_test, "version") == "3.11"
+    assert get_val(dict_for_test, "weather on monday") == "git"
+    assert get_val(dict_for_test, "weather on monday", "are you crazy?") == "are you crazy?"
